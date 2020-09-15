410903aabaf88a2d7d8d70699779c3e2e0cae6f9
vivekshukla <vivekshukla.nit@gmail.com>
2020-09-15 19:51:45 +0530

    Generate chatroom channel and add route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
 post 'message', to: 'messages#create'
+mount ActionCable.server, at: '/cable'
 end
