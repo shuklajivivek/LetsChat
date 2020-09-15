bac6f88d11e5fc23fdf3e6630707e0ffa387fa38
vivekshukla <vivekshukla.nit@gmail.com>
2020-09-15 20:15:45 +0530

    Displaying messages realtime

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,8 @@ class MessagesController < ApplicationController
     def create
       message = current_user.messages.build(message_params)
       if message.save
-        redirect_to root_path
+        ActionCable.server.broadcast "chatroom_channel",
+                                    mod_msg: message_render(message)
       end
     end
   
@@ -13,5 +14,9 @@ class MessagesController < ApplicationController
     def message_params
       params.require(:message).permit(:body)
     end
-  
+    def message_render(message)
+      #below line renders a partial
+      render(partial: 'message', locals: {message: message})
+      #locals: {message: message}) is passing message which we are getting at line 6 and passing that as a parameter to message partial
+    end
   end
